0cd2f343269bfe7efe08086f6e46db135322d6dc
gigya74 <gigya74@gmail.com>
2018-11-14 22:27:18 -0600

    Update GetCoordinates.py

--- google-api/src/GetCoordinates.py
+++ google-api/src/GetCoordinates.py
@@ -15,7 +15,7 @@ def init():
     Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
     filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
     doc = untangle.parse(filename)
-    apiKey = doc.root.GoogleAPIKey.cdata.strip()
+    apiKey = doc.root.myAPIKey.cdata.strip()
     host = doc.root.host.cdata.strip()
     addresses = doc.root.addresses.address
 def printLatLong():
@@ -23,7 +23,10 @@ def printLatLong():
     for address in addresses:
         a = address.cdata.strip() +" "+ address['CITY_STATE_ZIP']
         print(Fore.RESET+"The latitude and longiture for ",a," is:::"+Fore.CYAN)
-        print(get_coordinates(a))
+        coordinates = get_coordinates(a)
+        print(coordinates)
+        print(Fore.RESET+" Printing the formatted address by passing the coordinates to googles geocode api"+Fore.YELLOW)
+        print(reverseGeoCode(coordinates))
 
 def get_coordinates(address):
     """Gets the coordinates of addresses supplied in the config.xml by invoking google api"""
@@ -32,6 +35,17 @@ def get_coordinates(address):
     geo = response.json()
     results = geo['results'][0]
     return [results['geometry']['location']['lat'],results['geometry']['location']['lng']]
+
+def reverseGeoCode(coordinates):
+    """Gets the formatted address by using lat and long. Invokes Googles geocode api"""
+    c0 = repr(coordinates[0])
+    c1 = repr(coordinates[1])
+    url = host + "?latlng="+c0+','+c1+"&key=" + apiKey
+    response = requests.request('GET', url)
+    geo = response.json()
+
+    answer = geo['results'][0]
+    return answer.get('formatted_address')
 
 try:
     init()
